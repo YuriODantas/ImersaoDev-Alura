28f084e9cbd404dfb398d28b415d0f7fb7275c98
Yuri de Oliveira Dantas <iruy3343@gmail.com>
2021-04-18 18:46:14 -0300

    Create the basic functions (it's incomplete)

---
+++ calculadora/scripts/script.js
@@ -0,0 +1,13 @@
+let input = document.querySelector('.input')
+
+function calcMath(number){
+    input.value += number
+}
+
+function result(){
+    console.log("Precisa montar essa função")
+}
+
+function cc(){
+    input.value = ""
+}
