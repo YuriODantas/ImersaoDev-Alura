e3b38a973b3b58565e50f6aee38e0afb6d45f4ee
Yuri de Oliveira Dantas <iruy3343@gmail.com>
2021-04-19 12:55:59 -0300

    Create the functions in script.js

--- calculadora/scripts/script.js
+++ calculadora/scripts/script.js
@@ -1,13 +1,59 @@
-let input = document.querySelector('.input')
+let input = document.querySelector('.input');
 
-function calcMath(number){
-    input.value += number
+let cleanScreen = false;
+let operator = false;
+let point = false;
+let firstParameter = true;
+
+function addParameter(number) {
+    if (cleanScreen == true && number != '+' && number != '-' && number != '*' && number != '/' && number != '.') {
+        cc();
+        cleanScreen = false;
+    }
+
+    if (number == '+' || number == '-' || number == '*' || number == '/') {
+        if (operator == false && firstParameter == false) {
+            input.value += number;
+            operator = true;
+            point = false;
+            cleanScreen = false;
+        } else if (operator == true || firstParameter == true) {
+            input.value = input.value;
+        }
+    }
+
+    if(operator == true){
+        input.value = input.value;
+    }
+
+    if(number == '.'){
+        if(point == false && firstParameter == false){
+            input.value += number;
+            point = true;
+        }else{
+            input.value = input.value;
+        }
+    }
+
+    if(number != '+' && number != '-' && number != '*' && number != '/' && number != '.'){
+        input.value += number;
+        firstParameter = false;
+    }
 }
 
-function result(){
-    console.log("Precisa montar essa função")
+function result() {
+    let content = input.value;
+    if(content){
+        input.value = eval(content);
+    }
+    cleanScreen = true;
+    operator = false;
+    point = false;
 }
 
-function cc(){
-    input.value = ""
+function cc() {
+    input.value = "";
+    operator = false;
+    point = false;
+    firstParameter = true;
 }
