5d7eb70c13af948cd1ef976b590043743315f829
Yuri de Oliveira Dantas <iruy3343@gmail.com>
2021-04-22 17:08:00 -0300

    Bug fix

--- gameflix/scripts/script.js
+++ gameflix/scripts/script.js
@@ -3,15 +3,21 @@ let video = document.querySelector('#video');
 let listBanner = document.querySelector('.wrapper-banner');
 let newValue = 4;
 
+let validation = /(.jpg|.jpeg|.png|.gif)$/i;
+
 function addNewGame() {
     if (image.value == "" || video.value == "") {
         alert("Preencha todos os campos.");
     } else {
-        listBanner.innerHTML += `<a href=${video.value} target="_blank">
+        if(!validation.exec(image.value)){
+            alert("URL inválida. Preencha corretamente.")
+        }else{
+            listBanner.innerHTML += `<a href=${video.value} target="_blank">
             <img class="main-banner" src=${image.value} alt="banner ${newValue}">
             </a>`;
-        newValue++;
-        video.value = "";
-        image.value = "";
+            newValue++;
+            video.value = "";
+            image.value = "";
+        }
     }
 }
